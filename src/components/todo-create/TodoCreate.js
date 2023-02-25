6856b6db481d3e0d6c87c01a5e2cc3cfcb847c78
irgi <irgicimbrin@gmail.com>
2023-02-25 14:56:37 +0700

    react-cuy

---
+++ src/components/todo-create/TodoCreate.js
@@ -0,0 +1,36 @@
+import './TodoCreate.css';
+import {useState} from 'react';
+
+const TodoCreate = (props) => {
+    const [getInputTodo, setInputTodo] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newTodo ={
+            id: Math.floor(Math.random()* 100) + 1,
+            title: getInputTodo 
+        }
+        // console.log(newTodo);
+
+        props.onCreateTodo(newTodo);
+
+        setInputTodo('');
+        
+    }
+
+    const handleInputTodo = (event) => {
+        setInputTodo(event.target.value)
+        console.log(getInputTodo);
+        
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input type="text" value={getInputTodo} onChange={handleInputTodo}/>
+            <button type="submit">Add</button>
+        </form>    
+    )
+}
+
+
+export default TodoCreate;
